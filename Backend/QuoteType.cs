64d0ac6a812486002b919ed3f4aac6c38593806f
piggy <piggy>
2026-03-16 05:24:28 +1100

    if quotesfor start and end are same just pass start

--- Backend/QuoteType.cs
+++ Backend/QuoteType.cs
@@ -1,13 +1,13 @@
 namespace Backend;
 
-public class QuoteType(string start, string end)
+public class QuoteType(string start, string? end = null)
 {
     public string Start => start;
-    public string End => end;
+    public string End => end ?? start;
 
-    public static readonly QuoteType NoQuotes = new("", "");
-    public static readonly QuoteType SingleQuotes = new("'", "'");
+    public static readonly QuoteType NoQuotes = new("");
+    public static readonly QuoteType SingleQuotes = new("'");
     public static readonly QuoteType CurlySingleQuotes = new("‘", "’");
-    public static readonly QuoteType DoubleQuotes = new("\"", "\"");
+    public static readonly QuoteType DoubleQuotes = new("\"");
     public static readonly QuoteType CurlyDoubleQuotes = new("“", "”");
 }
