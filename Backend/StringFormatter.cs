4422100a7f67a2808259383ed640e4e9e75191d1
piggy <piggy>
2026-03-16 05:29:05 +1100

    change to file level namespace

--- Backend/StringFormatter.cs
+++ Backend/StringFormatter.cs
@@ -1,33 +1,32 @@
 ﻿using System.Text;
 
-namespace Backend
+namespace Backend;
+
+/*
+Improve a block of code as you see fit in C#.
+You may make any improvements you see fit, for example:
+  - Cleaning up code
+  - Removing redundancy
+  - Refactoring / simplifying
+  - Fixing typos
+  - Any other light-weight optimisation
+*/
+public static class StringFormatter
 {
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
-    public static class StringFormatter
+    //Code to improve
+    public static string ToCommaSeparatedList(this IEnumerable<string> items, QuoteType? quote = null)
     {
-        //Code to improve
-        public static string ToCommaSeparatedList(this IEnumerable<string> items, QuoteType? quote = null)
+        var quotesToUse = quote ?? QuoteType.NoQuotes;
+        var qry = new StringBuilder();
+        var isFirstItem = true;
+        foreach (var item in items)
         {
-            var quotesToUse = quote ?? QuoteType.NoQuotes;
-            var qry = new StringBuilder();
-            var isFirstItem = true;
-            foreach (var item in items)
-            {
-                var part = isFirstItem
-                    ? $"{quotesToUse.Start}{item}{quotesToUse.End}"
-                    : $", {quotesToUse.Start}{item}{quotesToUse.End}";
-                qry.Append(part);
-                isFirstItem = false;
-            }
-            return qry.ToString();
+            var part = isFirstItem
+                ? $"{quotesToUse.Start}{item}{quotesToUse.End}"
+                : $", {quotesToUse.Start}{item}{quotesToUse.End}";
+            qry.Append(part);
+            isFirstItem = false;
         }
+        return qry.ToString();
     }
 }
